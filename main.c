6e8efe56f12b1b259bba15f7c56d037d117540b3
Beoran <beoran@rubyforge.org>
2011-07-15 08:54:22 +0200

    Tutorial 2

--- main.c
+++ main.c
@@ -4,16 +4,43 @@
 
 #define MAX_SPRITES 64
 
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+int done = FALSE;
+  
+
 int main(void) {
   SDL_Surface * screen        = NULL;
   SDL_Surface * sprites[MAX_SPRITES];
-  SDL_Surface * player_sprite = NULL;
+  SDL_Surface * player_sprite = NULL; 
   // Friendly message.
   puts("Hello world!");
   // Start SDL
   SDL_Init(SDL_INIT_EVERYTHING);
   screen = SDL_SetVideoMode(640, 480, 0, SDL_HWSURFACE | SDL_ANYFORMAT);
   SDL_Delay(1000);
+  
+  while(!done) {
+    SDL_Event event;
+    /* Check for events */
+    /* Loop until there are no events left on the queue */
+    while(SDL_PollEvent(&event)) { 
+      switch(event.type) { /* Process the appropriate event type */
+      case SDL_QUIT:  /* Handle a KEYDOWN event */
+        done = TRUE;
+      break;
+      default: /* Report an unhandled event */
+        printf("I don't know what this event is!\n");
+      }
+    }
+  }
+ 
   
   // Quit SDL
   SDL_Quit(); 
